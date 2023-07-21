711f65f62e604f93847877e87cd0cc54c65a857d
Antonnoid <izhanton@gmail.com>
2023-07-21 16:59:46 +0300

    ko

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,17 +1,16 @@
 const router = require('express').Router();
 const rateRouter = require('./views/adminRate.route');
 const mainRouter = require('./views/main.route');
-const animalsRouter = require('./api/animals.route');
+// const animalsRouter = require('./api/animals.route');
 const adminRouter = require('./views/admin.route');
 const apiAdm = require('./api/apiAdminRate');
-const apiAnimalsRouter = require('./api/animals.route')
+const apiAnimalsRouter = require('./api/animals.route');
 // const authRouter = require('./views/auth.route');
 
 // router.use('/auth', authRouter);
 router.use('/', mainRouter);
 router.use('/admin', adminRouter);
 router.use('/adminRate', rateRouter);
-router.use('/animals', animalsRouter);
 router.use('/api/adminRate', apiAdm);
-router.use('api/animals', apiAnimalsRouter)
+router.use('api/animals', apiAnimalsRouter);
 module.exports = router;
